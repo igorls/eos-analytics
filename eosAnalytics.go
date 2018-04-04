97b291a392ce3a6860c3af8984dd8afb0d2ed78b
Igor Lins e Silva <igor.linsesilva@gmail.com>
2018-04-04 12:10:36 -0300

    dns resolution tracing

--- eosAnalytics.go
+++ eosAnalytics.go
@@ -10,10 +10,9 @@ import (
 )
 
 import (
-	"github.com/shirou/gopsutil/mem"
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/disk"
-	"strconv"
+	"sort"
+	"log"
+	"net/http/httptrace"
 )
 
 type GetInfoResponse struct {
@@ -58,6 +57,27 @@ func getJson(url string, target interface{}) (int64, error) {
 	return t, json.NewDecoder(r.Body).Decode(target)
 }
 
+func trace(url string) interface{} {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var ip interface{}
+	trace := &httptrace.ClientTrace{
+		DNSDone: func(dnsInfo httptrace.DNSDoneInfo) {
+			ip = dnsInfo.Addrs[0].IP
+		},
+		GotConn: func(connInfo httptrace.GotConnInfo) {
+			fmt.Printf("Got Conn: %+v\n", connInfo)
+		},
+	}
+	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+	if _, err := http.DefaultTransport.RoundTrip(req); err != nil {
+		log.Fatal(err)
+	}
+	return ip
+}
+
 func findPublicIP(server string) string {
 	resp, err := httpClient.Get(server)
 	if err != nil {
@@ -83,116 +103,82 @@ func avg(input []float64) float64 {
 	return total / float64(len(input))
 }
 
-func printUsage(u *disk.UsageStat) {
-	fmt.Println(u.Path + "\t" + strconv.FormatFloat(u.UsedPercent, 'f', 2, 64) + "% full.")
-	fmt.Println("Total: "  + strconv.FormatUint(u.Total/1024/1024/1024, 10) + " GiB")
-	fmt.Println("Free:  "  + strconv.FormatUint(u.Free /1024/1024/1024, 10) + " GiB")
-	fmt.Println("Used:  "  + strconv.FormatUint(u.Used /1024/1024/1024, 10) + " GiB")
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-
 func main() {
 	fmt.Println("EOS-Analytics")
 
-	v, _ := mem.VirtualMemory()
-	ram := float64(v.Total)/1000000000
-	fmt.Printf("Total System RAM: %.2f GB\n", ram)
-	fmt.Printf("UsedPercent: %.2f%%\n", v.UsedPercent)
-	fmt.Printf("------- CPU ------ \n")
-	c,_ := cpu.Info()
-	fmt.Printf("CPU Model: %v\n",c[0].ModelName)
-	fmt.Printf("CPU Clock: %v\n",c[0].Mhz)
-	fmt.Printf("CPU Cores: %v\n",c[0].Cores)
-	cpuUsage,_ := cpu.Percent(1,true)
-	fmt.Println(cpuUsage)
-	fmt.Printf("------- Storage ------ \n")
+	filepath := "nodes.json"
+	jsonFile, err := os.Open(filepath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened " + filepath)
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var nodeList NodeList
+	json.Unmarshal(byteValue, &nodeList)
 
-	parts, err := disk.Partitions(false)
-	check(err)
+	// check node's external ip address
+	externalIP := findPublicIP("http://myexternalip.com/raw")
+	fmt.Println("Firing requests from:", externalIP)
 
-	var usage []*disk.UsageStat
+	externalIP2 := findPublicIP("https://ipv4.icanhazip.com/")
+	fmt.Println("Firing requests from:", externalIP2)
 
-	for _, part := range parts {
-		u, err := disk.Usage(part.Mountpoint)
-		check(err)
-		usage = append(usage, u)
-		printUsage(u)
+	fmt.Println(len(nodeList.Nodes), "nodes on list")
+
+	for index, _ := range nodeList.Nodes {
+		nodeList.Nodes[index].Responses = []float64{}
 	}
 
-	//filepath := "testnets/jungle3.json"
-	//jsonFile, err := os.Open(filepath)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Println("Successfully Opened " + filepath)
-	//defer jsonFile.Close()
-	//byteValue, _ := ioutil.ReadAll(jsonFile)
-	//var nodeList NodeList
-	//json.Unmarshal(byteValue, &nodeList)
-	//
-	//// check node's external ip address
-	//externalIP := findPublicIP("http://myexternalip.com/raw")
-	//fmt.Println("Firing requests from:", externalIP)
-	//externalIP2 := findPublicIP("https://ipv4.icanhazip.com/")
-	//fmt.Println("Firing requests from:", externalIP2)
-	//
-	//fmt.Println(len(nodeList.Nodes), "nodes on list")
-	//
-	//for index, _ := range nodeList.Nodes {
-	//	nodeList.Nodes[index].Responses = []float64{}
-	//}
-	//
-	//for i := 0; i < 5; i++ {
-	//	for index, element := range nodeList.Nodes {
-	//		data := new(GetInfoResponse)
-	//		nodeURL := "http://" + element.NodeAddress + ":" + element.PortHTTP + "/v1/chain/get_info"
-	//		responseTime, err := getJson(nodeURL, &data)
-	//		if err != nil {
-	//			fmt.Println("Server is down")
-	//		} else {
-	//			//fmt.Println("Block:", data.HeadBlockNum)
-	//			//fmt.Println("Producer:", data.HeadBlockProducer)
-	//			respTime := float64(responseTime) / 1000000
-	//			fmt.Println("Latency:", respTime, "ms")
-	//			nodeList.Nodes[index].Responses = append(element.Responses, respTime)
-	//		}
-	//		fmt.Println("-------")
-	//	}
-	//}
-	//
-	//for index, element := range nodeList.Nodes {
-	//	fmt.Println(element.Name + " | " + element.Org)
-	//	fmt.Printf("%v\n", element.Responses)
-	//	if len(element.Responses) > 0 {
-	//		fmt.Println("Average Response Time:", avg(element.Responses))
-	//		nodeList.Nodes[index].AVR = avg(element.Responses)
-	//	}
-	//}
-	//
-	//sort.Slice(nodeList.Nodes, func(i, j int) bool {
-	//	return nodeList.Nodes[i].AVR < nodeList.Nodes[j].AVR
-	//})
-	//
-	//var output []Node
-	//for _, element := range nodeList.Nodes {
-	//	if len(element.Responses) > 0 {
-	//		output = append(output, element)
-	//	}
-	//}
-	//
-	//fmt.Println("----------------------------")
-	//fmt.Println("Fastest nodes for config.ini")
-	//fmt.Println("----------------------------")
-	//
-	//output = output[0:6]
-	//for _, element := range output {
-	//	fmt.Println("p2p-peer-address = " + element.NodeAddress + ":" + element.PortP2P)
-	//}
+	cycles := 1
+
+	for i := 0; i < cycles; i++ {
+		for _, element := range nodeList.Nodes {
+			nodeURL := "http://" + element.NodeAddress + ":" + element.PortHTTP + "/v1/chain/get_info"
+			remoteIP := trace(nodeURL)
+			fmt.Println("Remote IP:",remoteIP)
+			//data := new(GetInfoResponse)
+			//responseTime, err := getJson(nodeURL, &data)
+			//if err != nil {
+			//	fmt.Println("Server is down")
+			//} else {
+			//	//fmt.Println("Block:", data.HeadBlockNum)
+			//	//fmt.Println("Producer:", data.HeadBlockProducer)
+			//	respTime := float64(responseTime) / 1000000
+			//	fmt.Println("Latency:", respTime, "ms")
+			//	nodeList.Nodes[index].Responses = append(element.Responses, respTime)
+			//}
+			//fmt.Println("-------")
+		}
+	}
+
+	for index, element := range nodeList.Nodes {
+		fmt.Println(element.Name + " | " + element.Org)
+		fmt.Printf("%v\n", element.Responses)
+		if len(element.Responses) > 0 {
+			fmt.Println("Average Response Time:", avg(element.Responses))
+			nodeList.Nodes[index].AVR = avg(element.Responses)
+		}
+	}
+
+	sort.Slice(nodeList.Nodes, func(i, j int) bool {
+		return nodeList.Nodes[i].AVR < nodeList.Nodes[j].AVR
+	})
+
+	var output []Node
+	for _, element := range nodeList.Nodes {
+		if len(element.Responses) > 0 {
+			output = append(output, element)
+		}
+	}
+
+	fmt.Println("----------------------------")
+	fmt.Println("Fastest nodes for config.ini")
+	fmt.Println("----------------------------")
+
+	output = output[0:6]
+	for _, element := range output {
+		fmt.Println("p2p-peer-address = " + element.NodeAddress + ":" + element.PortP2P)
+	}
 
 }
